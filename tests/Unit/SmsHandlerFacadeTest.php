85216180a1414af9d132d99e5f56d7c7f25d3946
Moffat Munene <moffmu@gmail.com>
2024-09-01 15:39:36 +0300

    SMS update: set up tests

---
+++ tests/Unit/SmsHandlerFacadeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Moffhub\SmsHandler\Tests\Unit;
+
+use Moffhub\SmsHandler\Tests\TestCase;
+
+class SmsHandlerFacadeTest extends TestCase
+{
+    public function testCanCreateSmsHandlerFacade(): void
+    {
+        // arrange
+
+        // act
+
+        // assert
+        $this->assertTrue(true);
+    }
+}
